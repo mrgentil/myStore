48fd01e0d20d811e8d20e865144041bc9af13a50
mrgentil <tshitshob@gmail.com.com>
2024-05-29 16:16:07 +0100

    User Management Front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />} />
+
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
+);
